fd90745b268865138ea75d46b14841354a02aee8
Mo Aky <56073021+moe7863@users.noreply.github.com>
2025-05-29 09:34:43 +0100

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -5,6 +5,13 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive :D");
-
 // const interface = "Audio";
 // const private = 534; // reserved keywords
+
+// Function types: calling / running / invoking
+function logger() {
+  console.log("My name is Mohammad");
+}
+logger();
+logger();
+logger();
